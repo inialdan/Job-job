60f0d34ad3899d8685054af6e182ff334a51fb5b
Aldan Rizki Santosa <projectarizki@gmail.com>
2021-08-18 06:51:01 +0700

    Update .sql job job

--- @sql/job_job.sql
+++ @sql/job_job.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 04/06/2021 20:25:21
+ Date: 06/06/2021 15:47:48
 */
 
 SET NAMES utf8mb4;
@@ -28,7 +28,7 @@ CREATE TABLE `application`  (
   `email` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `phone` varchar(13) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `gender` varchar(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `experience` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `position` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `document` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `status` int(1) NOT NULL,
   `created_on` datetime(0) NOT NULL,
@@ -38,8 +38,8 @@ CREATE TABLE `application`  (
 -- ----------------------------
 -- Records of application
 -- ----------------------------
-INSERT INTO `application` VALUES (1, 1, 'Supriatna', 'gurunpasir@gmail.com', '0812345678990', 'L', 'No Document', 'default.pdf', 1, '2021-06-04 13:39:09');
-INSERT INTO `application` VALUES (2, 2, 'Abdul', 'gurunsahara@gmail.com', '0812345678990', 'P', 'No Document', 'default.pdf', 0, '2021-06-04 13:39:09');
+INSERT INTO `application` VALUES (1, 2, 'Latief', 'latief@gmail.com', '0000000', 'L', 'Operator', 'doc_60bb8dd72a0a1.jpg', 1, '2021-06-05 21:44:26');
+INSERT INTO `application` VALUES (2, 2, 'Latief', 'latief@gmail.com', '1234567890', 'L', 'Admin', 'doc_60bb8e06ec637.png', 2, '2021-06-05 21:45:15');
 
 -- ----------------------------
 -- Table structure for user
@@ -52,13 +52,16 @@ CREATE TABLE `user`  (
   `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `email` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `password` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `gender` varchar(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `avatar` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', 'admin', 'Admin Job Job', 'admin@jobjob.com', '$2y$10$SwhgQrsd08ZEh/9gJ8k77uW9FOVN28hdPSQngJ/0kPKQmyZgAZ0bG', 'default.png');
+INSERT INTO `user` VALUES (1, 'admin', 'admin', 'Admin Job Job', 'admin@jobjob.com', '$2y$10$SwhgQrsd08ZEh/9gJ8k77uW9FOVN28hdPSQngJ/0kPKQmyZgAZ0bG', 'L', 'default.png');
+INSERT INTO `user` VALUES (2, 'member', 'member', 'Latief', 'latief@gmail.com', '$2y$10$hkpmdZQjqJHkBUe2ZTeZiusTWsRxqhl5KItOqbzNENczv11ggAqZ6', 'L', 'default.png');
+INSERT INTO `user` VALUES (3, 'member', 'member2', 'Member 2', 'member2@gmail.com', '$2y$10$saM2KXmLx6FtbGkpb4CB7e/PCgHAZpAKfTgqwGdEJGAmtcOAaCU6O', 'P', 'default.png');
 
 SET FOREIGN_KEY_CHECKS = 1;
